45704b79d0fbbe63186c4f22d591856ec534daae
citrusbolt <32605812+citrusbolt@users.noreply.github.com>
2019-11-28 11:06:42 -0800

    Update board-autoconf.sh

--- piratebox/piratebox/bin/board-autoconf.sh
+++ piratebox/piratebox/bin/board-autoconf.sh
@@ -5,10 +5,8 @@ if [[ ! -d /opt/piratebox/share/board ]]; then
     echo "Run (as root):"
     echo "\t/opt/piratebox/bin/install_piratebox.sh /opt/piratebox/conf/piratebox.conf imageboard"
 else
-    echo -n "Imageboard admin password: "
-    read -s BOARDPASSWORD
-    echo
-    sed -i "s|xyzPASSWORDzyx|$BOARDPASSWORD|g" /opt/piratebox/share/board/config.pl
+    echo -n "Setting imageboard admin password"
+    sed -i "s|xyzPASSWORDzyx|PegLegBoard|g" /opt/piratebox/share/board/config.pl
 
     TEMPRAND=$(< /dev/urandom tr -dc A-Za-z0-9_ | head -c128)
     sed -i "s|xyzSECRETCODEzyx|$TEMPRAND|g" /opt/piratebox/share/board/config.pl
